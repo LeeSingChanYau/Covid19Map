68edcb8604e35971be22dbff1ad912d77cf7b7a8
LeeSingChanYau <67836976+LeeSingChanYau@users.noreply.github.com>
2020-08-06 12:03:02 -0400

    Updates

---
+++ precache-manifest.fd48b983614ba7115175f48b081daf24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52468bd2dee7d5b0c664357d6a043d7",
+    "url": "/Covid19Map/index.html"
+  },
+  {
+    "revision": "8604e750d6fbedb7cd2f",
+    "url": "/Covid19Map/static/css/main.ae3fab72.chunk.css"
+  },
+  {
+    "revision": "1544e4cb37b0a080ccba",
+    "url": "/Covid19Map/static/js/2.20b21bb6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Covid19Map/static/js/2.20b21bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8604e750d6fbedb7cd2f",
+    "url": "/Covid19Map/static/js/main.fec38e39.chunk.js"
+  },
+  {
+    "revision": "2777f55482185aac36b4",
+    "url": "/Covid19Map/static/js/runtime-main.2aaf5e3e.js"
+  }
+]);
